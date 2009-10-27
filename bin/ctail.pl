ac10eb69b1674184150804ddece34795793ee6f9
Steve Schnepp <steve.schnepp@pwkf.org>
2009-10-27 11:52:40 +0000

    - use GetOptLong - removed the "-f" flag since it's always following - removed warnings for scalar & "ne" comparison

--- bin/ctail.pl
+++ bin/ctail.pl
@@ -8,60 +8,65 @@ use Carp;
 use Data::Dumper;
 
 use IO::File;
+use Getopt::Long;
 
-my $isFollowing = 0;
+my $help;
+my $verbose;
 
-# Q&D Gestion des arguments
-while (my $arg = shift @ARGV) {
-	if ($arg !~ m/^\-/) {
-		# Fin des arguments, on remet dans la file
-		unshift @ARGV, $arg;
-		last;
-	}
-	
-	if ($arg eq "-f") {
-		$isFollowing = 1;
-	}
+my $result = GetOptions(
+        "help|h" => \$help,
+        "verbose|v" => \$verbose,
+);
+
+if ($help) {
+        print STDERR "Perl loose port of ctail(1) from http://ctail.i-want-a-pony.com/\n";
+        print STDERR "usage : $0 [host:]filename .. [host:]filename\n";
+        exit(1);
 }
 
-# Construct the array to "listen" to 
 my $files = {};
 foreach my $arg (@ARGV) {
 	my $cmd;
 	if ($arg =~ m/(.+):(.+)/) {
-		# It's  a ssh file
+		# It's an "ssh" file
 		$cmd = "ssh $1 tail -f $2";
 	} else {
+		# Regular file, just use a plain tail
 		$cmd = "tail -f $arg";
 	}
 
 	my $file = new IO::File("$cmd |");
-	$file->blocking(0);
 	$files->{$arg} = $file;
+
+	# Use non blocking IO, to be able to follow many streams at once
+	$file->blocking(0);
 }
 
-if ((scalar %$files) <= 0) {
-	exit;
+if ((scalar keys %$files) <= 0) {
+	# No file to follow
+	exit 0;
 }
 
 # listen to it
-my $current_file;
+my $current_file = ""; # Avoid undef. Empty filenames should not happen anyway.
 for (;;) {
-	my $isupdated = 0;
+	my $is_updated;
 	while (my ($filename, $file) = each %$files) {
 		my $line = $file->getline();
 		if (defined($line) && $line ne "") {
 			if ($current_file ne $filename) {
+				# Changed file
 				print "==> $filename <==\n";
 				$current_file = $filename
 			}
 			
 			print $line;
-			$isupdated = 1;
+			$is_updated ++;
 		}
 	}
 	
-	if (! $isupdated) {
+	if (! $is_updated) {
+		# Be CPU-friendly if nothing happened
 		sleep(1);
 	}
 }
