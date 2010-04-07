f0fa4e9b7c4cad5961f7e694b0cad81e4de2f12b
Steve Schnepp <steve.schnepp@pwkf.org>
2010-04-07 21:16:42 +0000

    - adding verbose - fixed initial truncate() - adding TODO

--- bin/rmbig.pl
+++ bin/rmbig.pl
@@ -1,7 +1,19 @@
 #! /usr/bin/perl
 # Removes a huge file without clogging up the I/O bandwidth
-# The idea borrowed from http://www.depesz.com/index.php/2010/04/04/how-to-remove-backups/
+# Idea borrowed from http://www.depesz.com/index.php/2010/04/04/how-to-remove-backups/
 # (c) 2010 LGPL - Steve Schnepp <steve.schnepp@pwkf.org>
+
+# TODO - Should implement these flags : 
+# rm generic flags : 
+#  -f, --force           ignore nonexistent files, never prompt
+#  -v, --verbose         explain what is being done
+#  -r, -R, --recursive   remove directories and their contents recursively
+#  --help      
+#  --version   
+
+# rmbig specific flags : 
+#  -s, --step-size       Use this step size [ default: 100MiB ] 
+#  -y, --yield-time      Yield time in ms [ default: 250ms ]
 
 use strict;
 use warnings;
@@ -11,13 +23,18 @@ use Time::HiRes qw( usleep );
 
 my $step = 100 * 1024 * 1024 * 1024; # 100 MiB increments
 my $yield_time_ms = 250; # yieald time to sleep in ms.
+my $verbose = 1; 
 
 foreach my $filename (@ARGV) {
-        my $stat = stat($filename);
-        for (my $current_size = $stat->size; $current_size < 0; $current_size -= $step) {
-                truncate($filename, $current_size);
+        print "removing $filename\n" if $verbose;
+
+        # truncate progressively to less than the smallest step
+        my $stat = stat($filename) or die($!);
+        for (my $current_size = $stat->size - $step; $current_size > 0; $current_size -= $step) {
+                truncate($filename, $current_size) or die($!);
                 usleep($yield_time_ms);
         }
+
         # finally unlink it
         unlink($filename);
 }
