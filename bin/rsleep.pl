c12d526bfbf33aaca1908471fa626f35be4cf515
Steve Schnepp <steve.schnepp@pwkf.org>
2010-05-11 12:54:40 +0000

    - Initial add of rsleep.pl

---
+++ bin/rsleep.pl
@@ -0,0 +1,43 @@
+#! /usr/bin/perl
+# Avoid crontab-induced load spikes by introducing random delays
+# 
+# Idea borrowed from Bjørn Hansen 
+# http://www.askbjoernhansen.com/2007/11/19/space_out_cronjobs.html
+#
+# (c) 2010 LGPL - Steve Schnepp <steve.schnepp@pwkf.org>
+
+use strict;
+use warnings;
+
+# Be able to have fractional second sleeps
+use Time::HiRes qw(sleep);
+
+my $time_human = shift;
+
+# Dictionary to convert units to seconds
+my $duration_in_sec = {
+	s => 1,
+	m => 60,
+	h => 60 * 60,
+	d => 24 * 60 * 60,
+};
+
+my $time_sec;
+if ($time_human =~ m/^([0-9.]+)([smhd])$/) {
+	# Convert the human-readable duration into 
+	# its second counterpart
+	$time_sec = $1 * $duration_in_sec->{$2};
+} else {
+	# Cannot read it. Take the numeric part 
+	# and assume it's seconds.
+	$time_sec = 0 + $time_human;
+}
+
+# Now sleep
+sleep($time_sec);
+
+# Nothing to exec, exit
+exit unless scalar @ARGV;
+
+# Asked to execute something after sleeping. 
+exec @ARGV;
