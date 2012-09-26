5b0a5f082fd15bbfe5f68a04d983ed65b108425f
Steve Schnepp <steve.schnepp@pwkf.org>
2012-09-26 15:54:26 +0200

    mktemp: initial add

---
+++ bin/mktemp.pl
@@ -0,0 +1,24 @@
+#! /usr/bin/perl
+# Replacement for mktemp(1) 
+
+use Getopt::Long;
+use File::Temp qw/ tempdir /;
+
+my $directory;
+GetOptions(
+        'directory|d' => \$directory,
+) or usage();
+
+my $template = shift;
+if ($directory) {
+        print File::Temp::tempdir($template);
+} else {
+        print File::Temp->new(TEMPLATE => $template, UNLINK => 0,);
+}
+print "\n";
+
+sub usage {
+        print "mktemp [-d] <template>\n";
+        exit 1;
+}
+
